0549ff93e9197e72923feee677a911b520d84c8d
Semyon Kirekov <kirekov@gmail.com>
2017-12-18 12:30:04 +0700

    Добавлена возможность формирования отчетов в html и excel

--- BD7/Reports.cs
+++ BD7/Reports.cs
@@ -153,7 +153,47 @@ namespace BD7
                 writer.Write(html);
                 writer.Close();
                 MessageBox.Show("Отчет успешно сохранен");
+                System.Diagnostics.Process.Start(saveFileDialog1.FileName);
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // creating Excel Application  
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            // creating new WorkBook within Excel application  
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            // creating new Excelsheet in workbook  
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            // get the reference of first sheet. By default its name is Sheet1.  
+            // store its reference to worksheet  
+            worksheet = workbook.Sheets["Лист1"];
+            worksheet = workbook.ActiveSheet;
+            // changing the name of active sheet  
+            worksheet.Name = "Exported from gridview";
+            // storing header part in Excel  
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            // storing Each row and column value to excel sheet  
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+
+            saveFileDialog1.Filter = "Excel таблиы|*.xls";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                workbook.SaveAs(saveFileDialog1.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                MessageBox.Show("Отчет успешно сохранен");
+                
+            }
+            app.Quit();
+            System.Diagnostics.Process.Start(saveFileDialog1.FileName);
         }
     }
 }
